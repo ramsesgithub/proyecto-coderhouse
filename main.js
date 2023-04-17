bcf36c6af07405444a87eae319ca164bc60f91d5
ramsesgithub <127460936+ramsesgithub@users.noreply.github.com>
2023-04-17 15:39:00 -0300

    ejercicios galeria

---
+++ main.js
@@ -0,0 +1,6 @@
+const ejercicio=document.getElementById("click-eje");
+ejercicio.addEventListener("click", alerta);
+
+function alerta(){
+    alert("Haciendo click te llevaria a un menu con videos de esos ejercicos o a una lista de reproduccion...");
+}
