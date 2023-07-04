4f3e926b68e1e02e008219a177d45bff8c5901da
Ramses <127460936+ramsesgithub@users.noreply.github.com>
2023-07-04 12:30:19 -0300

    modificando inputs

--- main.js
+++
@@ -1,6 +0,0 @@
-const ejercicio=document.getElementById("click-eje");
-ejercicio.addEventListener("click", alerta);
-
-function alerta(){
-    alert("Haciendo click te llevaria a un menu con videos de esos ejercicos o a una lista de reproduccion...");
-}
